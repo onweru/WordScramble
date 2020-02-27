5c08c1eeac2e995ebc647c578ce1013763424c0c
weru <onewesh@gmail.com>
2020-02-27 17:19:14 +0300

    save work

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -9,8 +9,125 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var triedWords = [String]()
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .autocapitalization(.none)
+                
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+                
+                Section(header: Text("Used words")) {
+                    List(triedWords, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        .navigationBarTitle(rootWord)
+                .navigationBarItems(leading: Button ("Start Game") {
+                    self.startGame()
+                })
+        .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("Ok")))
+            }
+        }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased()
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "Can't make them as you go!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", message: "That's not a real word")
+            return
+        }
+        
+        guard isDifferent(word: answer) else {
+            wordError(title: "Word is identical", message: "Be a little creative")
+            return
+        }
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n"
+                )
+                rootWord = allWords.randomElement() ?? "silkworm"
+                triedWords.append(rootWord)
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord.lowercased()
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func isDifferent(word: String) -> Bool {
+       return word.lowercased() != rootWord.lowercased()
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
